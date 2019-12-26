5cb318f608b9b09e984f399dcef0e202c47d5dec
Lianzhen <lianzhenw01@gmail.com>
2019-12-26 21:02:52 +0800

    version_03

--- password_input.py
+++ password_input.py
@@ -1,12 +1,16 @@
 password = 'a123456'
-x = 3
-while True:
+x = 3 #剩餘輸入次數
+
+while x > 0:
+	x = x - 1
 	keyin = input('請輸入密碼: ')
 	if keyin == password:
 		print('登入成功')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤! 還有', x, '次機會')
-		if x == 0:
-			break
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('帳號被鎖了')
+		
